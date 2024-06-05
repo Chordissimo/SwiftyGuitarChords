c052a7b8dacdfbe7eac4ab6548e57c3d7ae20d9e
Antons Andrejevs <anton.andreyeff@gmail.com>
2024-06-05 08:34:25 +0300

    color bugfix

--- Sources/SwiftyChords/Aliasses.swift
+++ Sources/SwiftyChords/Aliasses.swift
@@ -40,13 +40,13 @@ public typealias SWIFTFont = UIFont
 //let primaryColor = UIColor.label
 //let backgroundColor = UIColor.systemBackground
 
-let primaryColor = UIColor(hex: 0x454647)
-let backgroundColor = UIColor(hex: 0x303133)
-let stringColor = UIColor(hex: 0x6E6F70)
-let fretColor = UIColor(hex: 0x1D1D1D)
-let nutColor = UIColor(hex: 0x929394)
-let dotColor = UIColor(hex: 0x19C63E)
-let dotLabelColor = UIColor(hex: 0x121212)
+let primaryColor = UIColor(hex: "454647")
+let backgroundColor = UIColor(hex: "303133")
+let stringColor = UIColor(hex: "6E6F70")
+let fretColor = UIColor(hex: "1D1D1D")
+let nutColor = UIColor(hex: "929394")
+let dotColor = UIColor(hex: "19C63E")
+let dotLabelColor = UIColor(hex: "121212")
 
 
 
